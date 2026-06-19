d57812ec6ba4da17e684d37ccc1259b3e953e8d0
Mickaël GARNIER <mickaelgar@gmail.com>
2026-06-19 10:58:57 +0200

    use broadcast and all users have message of /close and /part

--- srcs/Commands.cpp
+++ srcs/Commands.cpp
@@ -475,11 +475,13 @@ void Server::CommandClose(std::istringstream &iss, int clientFd)
     cit->second.removeOperator(clientFd);
     std::string msg = ":" + _clients[j].getName() + "!" + _clients[j].getUsername() + "@localhost" + " PART " + cit->second.getName() + "\r\n";
     send(clientFd, msg.c_str(), msg.size(), 0);
+    cit->second.broadcast(msg.c_str());
     if (cit->second.memberCount() == 0)
     {
         _channels.erase(cit);
         std::cout << "channel erase (0 member)"  << std::endl;
     }
+
 }
 
 void Server::CommandMode(std::istringstream &iss, int clientFd)
--- toDoList.md
+++ toDoList.md
@@ -9,7 +9,8 @@
 comprendre l'utilisation de errno apres le poll (pour le sujet de correction)
 Enlever les debugs.
 Co^Dmm^Dand^D
-
+Oper met en mode invite puis quitte = channel bloqué de l'intérieur
+/quit, /close et /part ne mettent pas de messages dans les channels de leur départ
 bot.
  :is unknown mode char apparait au debut
 # end
