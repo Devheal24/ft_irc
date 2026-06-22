80ee8b768c5b87845def6aa50d584960166499ee
Mickaël GARNIER <mickaelgar@gmail.com>
2026-06-22 14:52:57 +0200

    fix segfault on Quit.cpp because of the iterator, it was lost

--- srcs/Commands/Quit.cpp
+++ srcs/Commands/Quit.cpp
@@ -7,13 +7,17 @@ void Server::CommandQuit(std::istringstream &iss, int clientFd)
 	for (size_t i = 0; i < _fds.size(); i++)
 		if (_fds[i].fd == clientFd)
 			_fds.erase(_fds.begin() + i);
-	for (std::map<std::string, Channel>::const_iterator it = _channels.begin(); it != _channels.end(); ++it)
+	for (std::map<std::string, Channel>::const_iterator it = _channels.begin(); it != _channels.end();)
 	{
 		if (it->second.hasMember(clientFd))
 		{
 			client->setActiveChannel(it->second.getName());
+			++it;
 			CommandClose(iss, clientFd);
+
 		}
+		else
+			++it;
 	}
 	std::string msg = ":server 901 " + client->getName() + " " + client->getUsername() + "@localhost" + " :You are now logged out\r\n";
 	send(clientFd, msg.c_str(), msg.size(), 0);
